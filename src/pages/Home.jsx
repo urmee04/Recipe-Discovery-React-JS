949d2990b2daaf33d1a20ae2b0fa8c4ac7e3c076
urmee04 <urmee04sust@gmail.com>
2025-08-09 13:17:44 -0400

    create main Home page layout with welcome message

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div>
+      <div className="main">
+        <div className="header"></div>
+        <h2>Welcome to Recipe Discovery</h2>
+        <h4>
+          Welcome to Recipe Discovery: An open, crowd-sourced database of
+          recipes from around the world.
+        </h4>
+      </div>
+    </div>
+  );
+}
